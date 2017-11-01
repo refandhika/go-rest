067df1e7762999f7aeec88bbeb3e820f86ae07f1
refandhika <refandika@gmail.com>
2017-11-01 21:25:35 +0700

    finising init

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// Create New Type
 type Person struct {
 	ID string `json:"id,omitempty"`
 	Firstname string `json:"firstname,omitempty"`
@@ -19,12 +20,46 @@ type Address struct {
 	State string `json:"state,omitempty"`
 }
 
+// Global Init
 var people []Person
 
+// Endpoints
 func GetPeople(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(people)
 }
 
+func GetPerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _,item := range people {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Person{})
+}
+
+func CreatePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
+}
+
+func DeletePerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index,item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(people)
+}
+
+// Main
 func main() {
 	router := mux.NewRouter()
 
@@ -33,9 +68,9 @@ func main() {
 	people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
 
 	router.HandleFunc("/people", GetPeople).Methods("GET")
-	//router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
-	//router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
-	//router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
+	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
+	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
+	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000",router))
 }
